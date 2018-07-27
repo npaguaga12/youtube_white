ce150eaf833e9613c6a4cbb181896378c9f6d3bc
Natalia Paguaga <npaguaga12@gmail.com>
2018-07-26 23:21:50 -0400

    coding challenge update

--- Desktop/hacker_news/data/datasources/hacker_news.py
+++ Desktop/hacker_news/data/datasources/hacker_news.py
@@ -1,23 +1,37 @@
 """HackerNews Datasource
 
-api is documented here: https://github.com/HackerNews/API
-"""
+api is documented here: https://github.com/HackerNews/API"""
+
 import requests
+
+response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+response.status_code
+data = response.json()
+data = data[0:10]
+data """check if 10 topstory ids are stored there"""
+
+url_template = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'
+[item for item in data]
+[url_template.format(item) for item in data]
+urls = _ """save result url from topstories in variable"""
+
+results = []
+for url in urls:
+    response = requests.get(url)
+    results.append(response.json())
+    print(results)
+
+titles = [t['title'] for t in results if 'title' in t]
+print(titles)
+
+authors = [a['by'] for a in results if 'by' in a]
+print(authors)
+
+"""loop sequences using zip() function"""
+for t, a, u in zip(titles, authors, urls):
+    print('{0}: {1}, {2}'.format(t, a, u))
+
 
 
 class HackerNewsDatasource(object):
     """Handles all communication with HackerNews"""
-
-    def get_topstory_ids(self):
-        """Returns a list of current top story ids."""
-        response = requests.get(
-            'https://hacker-news.firebaseio.com/v0/topstories.json')
-        return response.json()
-
-    def get_story(self, id):
-        """Fetches and returns a story."""
-        raise NotImplementedError()
-
-    def get_top_stories(self):
-        """Fetches and returns top stories."""
-        raise NotImplementedError()
--- Desktop/hacker_news/fetch_data.py
+++ Desktop/hacker_news/fetch_data.py
@@ -5,6 +5,7 @@ import argparse
 from data.datasources import giphy
 
 
+
 def main(datasource, raw, file_dest):
     if not file_dest:
         if datasource == 'giphy':
